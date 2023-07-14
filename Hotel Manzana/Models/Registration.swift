148cd8323fe32fad41008ff90003bcf67d6e301f
jonsalkin <jon.salkin@gmail.com>
2023-07-14 18:30:03 -0400

    Total app commit.

---
+++ Hotel Manzana/Models/Registration.swift
@@ -0,0 +1,25 @@
+//
+//  Registration.swift
+//  Hotel Manzana
+//
+//  Created by Jon Salkin on 7/11/23.
+//
+
+import Foundation
+
+struct Registration {
+    var firstName: String
+    var lastName: String
+    var emailAddress: String
+    
+    var checkInDate: Date
+    var checkOutDate: Date
+    var numberOfAdults: Int
+    var numberOfChildren: Int
+    
+    var wifi: Bool
+    var roomType: RoomType
+}
+
+
+
